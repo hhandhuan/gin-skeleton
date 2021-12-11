3289447b151443ddcb40dfcd4837bdfc86a197f1
huhaophp <95403191+hhandhuan@users.noreply.github.com>
2021-12-11 23:35:03 +0800

    Add request form

--- internal/request/request.go
+++ internal/request/request.go
@@ -2,11 +2,11 @@ package request
 
 type (
 	CreateAuthTokenRequest struct {
-		Username string `json:"username" binding:"required"` // 登陆用户
-		Password string `json:"password" binding:"required"` // 登陆密码
+		Username string `json:"username" form:"username" binding:"required"` // 登陆用户
+		Password string `json:"password" form:"username" binding:"required"` // 登陆密码
 	}
 	EditUserRequest struct {
-		Username string `json:"username" binding:"required"` // 登录名
-		Nickname string `json:"nickname" binding:"required"` // 用户别名
+		Username string `json:"username" form:"username" binding:"required"` // 登录名
+		Nickname string `json:"nickname" form:"username" binding:"required"` // 用户别名
 	}
 )
