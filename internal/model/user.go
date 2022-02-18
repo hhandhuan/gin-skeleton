e35ea7ffbe4bbb38a25f7ae5606c6c1c011804ac
huhaophp <95403191+hhandhuan@users.noreply.github.com>
2022-02-18 15:28:01 +0800

    Update user.go

--- internal/model/user.go
+++ internal/model/user.go
@@ -5,6 +5,6 @@ type User struct {
 	Email     string `json:"phone"`      // 用户手机
 	Password  string `json:"-"`          // 用户密码
 	State     int    `json:"state"`      // 用户呢称
-	CreatedAt *cTime `json:"created_at"` // 创建时间
-	UpdatedAt *cTime `json:"updated_at"` // 更新时间
+	CreatedAt *gTime `json:"created_at"` // 创建时间
+	UpdatedAt *gTime `json:"updated_at"` // 更新时间
 }
