3038ae3c3a72e47018a12f153b021e8eb259ed43
huhaophp <95403191+hhandhuan@users.noreply.github.com>
2022-02-18 15:14:42 +0800

    Update model.go

--- internal/model/model.go
+++ internal/model/model.go
@@ -6,18 +6,18 @@ import (
 	"time"
 )
 
-type cTime struct {
+type gTime struct {
 	time.Time
 }
 
-// MarshalJSON on cTime format Time field with %Y-%m-%d %H:%M:%S
-func (t cTime) MarshalJSON() ([]byte, error) {
+// MarshalJSON on gTime format Time field with %Y-%m-%d %H:%M:%S
+func (t gTime) MarshalJSON() ([]byte, error) {
 	formatted := fmt.Sprintf("\"%s\"", t.Format("2006-01-02 15:04:05"))
 	return []byte(formatted), nil
 }
 
 // Value insert timestamp into mysql need this function.
-func (t cTime) Value() (driver.Value, error) {
+func (t gTime) Value() (driver.Value, error) {
 	var zeroTime time.Time
 	if t.Time.UnixNano() == zeroTime.UnixNano() {
 		return nil, nil
@@ -26,10 +26,10 @@ func (t cTime) Value() (driver.Value, error) {
 }
 
 // Scan valueof time.Time
-func (t *cTime) Scan(v interface{}) error {
+func (t *gTime) Scan(v interface{}) error {
 	value, ok := v.(time.Time)
 	if ok {
-		*t = cTime{Time: value}
+		*t = gTime{Time: value}
 		return nil
 	}
 	return fmt.Errorf("can not convert %v to timestamp", v)
